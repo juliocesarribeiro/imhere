82bea72aa6d64084dfc07b6b833de4f156b476d0
Julio Cesar Ribeiro <juliooriebir@gmail.comclear>
2024-11-17 09:49:29 -0300

    :sparkles: (feat): Added the function to remove participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,28 +5,32 @@ import { Participant } from '../../components/Participant';
 
 import { styles } from './styles';
 export function Home() {
-  const [participants, setParticipants] = useState(['Julio']);
+  const [participants, setParticipants] = useState<String[]>([]);
+  const [participantNames, setParticipantNames] = useState('');
   const agora = new Date();
 
   function handleParticipantAdd() {
-    if (participants.includes('Rodrigo')) {
-      return Alert.alert(`Participante já existe,`)
+    if (participants.includes(participantNames)) {
+      return Alert.alert('Novo Participante', `Participante ${participantNames} já existe !`)
+    } if (participantNames === '') {
+      return Alert.alert('Obrigátorio inserir o nome do participante !')
     }
-    setParticipants(prevState => [...prevState, 'Ana']);
+    setParticipants(prevState => [...prevState, participantNames]);
+    setParticipantNames('');
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert("Remove", `Deseja remover o participante ${name} ?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert("Deletado")
+        onPress: () => setParticipants(prevState => prevState.filter(participants => participants !== name)),
       },
       {
         text: 'Não',
-        onPress: () => console.log('Cancelado'),
+        onPress: () => console.log('Operação Cancelada pelo usuário'),
         style: 'cancel',
       }
-    ])
+    ]);
   }
 
   return (
@@ -37,14 +41,17 @@ export function Home() {
       </Text>
 
       <Text style={styles.eventDate} >
-        Data:{agora.toLocaleDateString()}
+        Data: {agora.toLocaleDateString()}
       </Text>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
           placeholder='Nome do participante'
           placeholderTextColor={"#6b6b6b"}
+          onChangeText={setParticipantNames}
+          value={participantNames}
         />
+
         <TouchableOpacity
           style={styles.button}
           onPress={handleParticipantAdd}>
@@ -56,7 +63,7 @@ export function Home() {
 
       <FlatList
         data={participants}
-        keyExtractor={item => item}
+        keyExtractor={(item: string) => item}
         renderItem={({ item }) => (
           <Participant
             key={item}
@@ -71,8 +78,6 @@ export function Home() {
           </Text>
         )}
       />
-
-
 
     </View>
   );
